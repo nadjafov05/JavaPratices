c79adb80de3c80fa275d64a564409a1ddfce3234
Perviz Necefov <nadjafovp@gmail.com>
2025-06-29 19:32:21 +0400

    java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+
+        System.out.println(a);
+
+        a = a + 2; // 3
+
+        a += 2; // 5
+
+        System.out.println(a);
+    }
+}
