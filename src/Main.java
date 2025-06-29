26902129791af1e1333814605b9918493420b3ee
Perviz Necefov <nadjafovp@gmail.com>
2025-06-29 21:40:58 +0400

    java2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,25 @@ public class Main {
     }
 
 
+    public static void logisticalOperators(){
+        Scanner enterNumbersUser = new Scanner(System.in);
+
+        System.out.println("birinci reqemi daxil edin: ");
+        int a = enterNumbersUser.nextInt();
+
+        System.out.println("ikinci reqemi daxil edin: ");
+        int b = enterNumbersUser.nextInt();
+
+        if (a >= 0 && b >= 0){
+            System.out.println("her iksi musbetdir");
+        }else if (a<=0 && b <= 0){
+            System.out.println("her iksi menfidir");
+        }else if (a <= 0 || b <= 0){
+            System.out.println("en azi biri sifirdir");
+        }else if (!(a>=0)){
+            System.out.println("birinci reqem musbet deyil");
+        }
+    }
     public static void RelationalOperators(){
         int a = 6;
         int b = 2;
