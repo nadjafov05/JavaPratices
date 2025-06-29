3e7602721a8011644212bb83397a0c2f03ad7bc1
Perviz Necefov <nadjafovp@gmail.com>
2025-06-29 22:07:45 +0400

    ternaryOperators

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,39 @@ public class Main {
     }
 
 
+    public static void ternaryOperators(){
+        int a = 0;
+        String netice = ((a>0)? "Musbet ededdir" : "Menfi ededdir");
+        System.out.println(netice);
+
+        int b = 7;
+        int c = 11;
+        int netice1 = ((b>c)? b : c);
+        System.out.println(netice1);
+
+        int enterBrithday = 15;
+        String resultBrithday = ((enterBrithday>18)? "18 yasiniz var" : "18 yasiniz yoxdur");
+        System.out.println(resultBrithday);
+    }
+    public static void assignmentOperators(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("bir eded daxil edin: ");
+        int a = sc.nextInt();
+        a += 10;
+        System.out.println(a);
+
+        a -= 5;
+        System.out.println(a);
+
+        a *= 3;
+        System.out.println(a);
+
+        a /= 2;
+        System.out.println(a);
+
+        a%=4;
+        System.out.println(a);
+    }
     public static void logisticalOperators(){
         Scanner enterNumbersUser = new Scanner(System.in);
 
@@ -25,7 +58,7 @@ public class Main {
             System.out.println("birinci reqem musbet deyil");
         }
     }
-    public static void RelationalOperators(){
+    public static void relationalOperators(){
         int a = 6;
         int b = 2;
 
